3c7fa8881d28d081183fcf9417d66bf1c29764f9
Zakia Sultana <128232277+ZakiaSultana4@users.noreply.github.com>
2024-06-04 21:52:43 +0600

    successfully added scholarship and save in mongodb

--- src/Pages/Checkout/SuccessApply.jsx
+++ src/Pages/Checkout/SuccessApply.jsx
@@ -62,7 +62,7 @@ const SuccessApply = () => {
         </h2>
 
         <form onSubmit={handleFormSubmit}>
-          <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+          <div className="grid grid-cols-1 gap-6 mt-5 sm:grid-cols-2">
             <div>
               <label className="text-gray-700 " htmlFor="Name">
                 Applicant Name
