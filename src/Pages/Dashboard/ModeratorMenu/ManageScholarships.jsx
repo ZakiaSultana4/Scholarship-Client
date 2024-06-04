252b3e976f2a41c8525c8496529986058ef60368
Zakia Sultana <128232277+ZakiaSultana4@users.noreply.github.com>
2024-06-04 21:47:59 +0600

    manage all scholarship  section added

--- src/Pages/Dashboard/ModeratorMenu/ManageScholarships.jsx
+++ src/Pages/Dashboard/ModeratorMenu/ManageScholarships.jsx
@@ -36,7 +36,7 @@ const ManageScholarships = () => {
   console.log(scholarship);
 
   return (
-    <section className="container px-4 mx-auto pt-12">
+    <section className="container px-4 mx-auto pt-10">
       <div className="flex items-center gap-x-3">
         <h2 className="text-lg font-medium text-gray-800 ">My scholarship</h2>
 
