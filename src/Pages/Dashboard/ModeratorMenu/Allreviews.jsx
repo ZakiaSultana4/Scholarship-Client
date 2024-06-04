dfb72de2b2a8ed7ae1cad306f49f72cbbb3833e1
Zakia Sultana <128232277+ZakiaSultana4@users.noreply.github.com>
2024-06-04 21:53:34 +0600

    successfully added review

--- src/Pages/Dashboard/ModeratorMenu/Allreviews.jsx
+++ src/Pages/Dashboard/ModeratorMenu/Allreviews.jsx
@@ -34,7 +34,7 @@ const Allreviews = () => {
     }
   };
   return (
-    <section className="container px-4 mx-auto pt-12">
+    <section className="container px-4 mx-auto pt-10">
     <div className="flex items-center gap-x-3">
       <h2 className="text-lg font-medium text-gray-800 ">
         My Posted reviews in perticular Scholarship
