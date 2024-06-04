9537115c387b8e23e21f7f950c2123c03ad93bec
Zakia Sultana <128232277+ZakiaSultana4@users.noreply.github.com>
2024-06-04 21:32:56 +0600

    slider  section  added

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -16,12 +16,7 @@ const Slider = ({ image, text ,p}) => {
           </h1>
           <p className='text-3xl font-semibold text-white lg:text-xl'>{p}</p>
           <br />
-          <Link
-            to='/add-job'
-            className='w-full px-5 py-4 mt-4 text-sm font-medium text-white capitalize transition-colors duration-300 transform bg-gray-600 rounded-md lg:w-auto hover:bg-gray-500 focus:outline-none focus:bg-gray-500'
-          >
-            Post Job & Hire Expert
-          </Link>
+      
         </div>
       </div>
     </div>
